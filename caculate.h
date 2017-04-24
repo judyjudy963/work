bdd5082f4dd476a20078b476abbf657adb3a3f3c
judyjudy963 <1115172705@qq.com>
2017-04-24 22:36:49 +0800

    Create caculate.h
    计算加减乘法，及比较大小，找出大的

---
+++ caculate.h
@@ -0,0 +1,18 @@
+#include "include/calculator.h"
+int sum(int x, int y) 
+{	
+return x + y;
+}
+int subtract(int x, int y) 
+{	
+return x - y;
+}
+int cheng(int x, int y) 
+{	
+return x * y;
+}
+int max(int x, int y) 
+{	
+  if(x>=y) return x;
+  else     return y;
+}
