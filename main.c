aa6bf50cd0262cd0f5cf3f72b13a2c70d000dc4a
judyjudy963 <1115172705@qq.com>
2017-04-25 00:27:38 +0800

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main(void)
 {	
    int x, y;
 	 scanf("%d%d",&x,&y);
-         printf("%d * %d) = %d\n", x, y, cheng(5, 6));
-	 printf("tne max is %d\n",max(5,6));
+         printf("%d * %d) = %d\n", x, y, cheng(x, y));
+	 printf("tne max is %d\n",max(x,y));
 	 return 0;
   }
