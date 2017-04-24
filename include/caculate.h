c826dfc73d98a8fbf8f234974e198361466076da
judyjudy963 <1115172705@qq.com>
2017-04-24 22:58:21 +0800

    Update caculate.h

--- include/caculate.h
+++ include/caculate.h
@@ -2,6 +2,7 @@
 #define _CALC_H
 int sum(int x, int y);
 int subtract(int x, int y);
-int multiply(int x, int y);
+int cheng(int x, int y);
 int max(int x, int y);
+int min(int x, int y);
 #endif //_CALC_H
