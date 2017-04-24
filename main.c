f3cd706757ff75a08390049a25318ca01add8e19
judyjudy963 <1115172705@qq.com>
2017-04-24 23:01:58 +0800

    Update main.c

--- main.c
+++ main.c
@@ -2,9 +2,9 @@
 #include "include/calculator.h"
 int main(void) 
 {	
-   int x, y;		
-   x = 5;	
-   y = 6;
-	 printf("%d * %d) = %d\n", x, y, cheng(5, 6));
+   int x, y;
+	 scanf("%d%d",&x,&y);
+         printf("%d * %d) = %d\n", x, y, cheng(5, 6));
+	 printf("tne max is %d\n",max(5,6));
 	 return 0;
   }
